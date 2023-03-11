361d278aca942d815a85d0fa8ff459c3d8391f33
jackdevilz <44961153+jackdevilz@users.noreply.github.com>
2023-03-11 19:11:29 +0800

    Add files via upload
    Signed-off-by: jackdevilz <44961153+jackdevilz@users.noreply.github.com>

--- lib/widget/wallet/wallet_tab_button.dart
+++ lib/widget/wallet/wallet_tab_button.dart
@@ -1,1 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:jackdigitalstore_app/provider/wallet_provider.dart';
+import 'package:jackdigitalstore_app/utils/shared.dart';
 
+class WalletTabButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final listTab =
+        Provider.of<WalletProvider>(context, listen: false).tabWallet;
+    final selectedTab = Provider.of<WalletProvider>(context).selectedTab;
+
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 15),
+      child: GridView.builder(
+        primary: false,
+        shrinkWrap: true,
+        itemCount: listTab!.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisSpacing: 8,
+            mainAxisSpacing: 8,
+            crossAxisCount: 3,
+            childAspectRatio: 2 / 1),
+        itemBuilder: (context, i) {
+          return InkWell(
+            child: Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  color: selectedTab == listTab[i]
+                      ? accentColor
+                      : Colors.grey[200],
+                  borderRadius: BorderRadius.circular(8)),
+              child: Text(
+                Provider.of<WalletProvider>(context)
+                    .typeToTitle(listTab[i], context)!,
+                style: TextStyle(
+                    fontSize: 12,
+                    color: selectedTab == listTab[i]
+                        ? Colors.white
+                        : Colors.black),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            onTap: () {
+              Provider.of<WalletProvider>(context, listen: false)
+                  .onTabChange(listTab[i]);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/widget/wallet/wallet_transaction_item.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:jackdigitalstore_app/model/wallet/wallet_model.dart';
+import 'package:jackdigitalstore_app/utils/currency_format.dart';
+import 'package:shimmer/shimmer.dart';
+
+class WalletTransactionItem extends StatelessWidget {
+  final WalletModel? transaction;
+  final bool? loading;
+  WalletTransactionItem({this.transaction, this.loading});
+
+  final f = DateFormat('dd MMMM, yyyy');
+
+  @override
+  Widget build(BuildContext context) {
+    var _date = f.format(DateTime.parse(transaction!.date!));
+    if (loading == true) {
+      return Shimmer.fromColors(
+        baseColor: Colors.grey[300]!,
+        highlightColor: Colors.grey[100]!,
+        child: ListTile(
+          title: Container(
+            height: 10,
+            width: 10,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8), color: Colors.white),
+          ),
+          subtitle: Container(
+            height: 10,
+            width: 15,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8), color: Colors.white),
+          ),
+          trailing: Container(
+            height: 30,
+            width: 30,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8), color: Colors.white),
+          ),
+        ),
+      );
+    }
+    return Container(
+      child: ListTile(
+        title: Text(transaction!.detail!),
+        subtitle: Text(_date),
+        trailing: Text(
+          transaction!.type! == 'credit'
+              ? "+${stringToCurrency(double.parse(transaction!.amount!), context)}"
+              : "-${stringToCurrency(double.parse(transaction!.amount!), context)}",
+          style: TextStyle(
+              color:
+                  transaction!.type! == 'credit' ? Colors.green : Colors.red),
+        ),
+      ),
+    );
+  }
+}
