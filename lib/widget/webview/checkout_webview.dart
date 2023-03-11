36faed6414d657f42fd3189460c72e547d09e75e
jackdevilz <44961153+jackdevilz@users.noreply.github.com>
2023-03-11 19:09:58 +0800

    Add files via upload
    Signed-off-by: jackdevilz <44961153+jackdevilz@users.noreply.github.com>

--- lib/widget/webview/checkout_webview.dart
+++ lib/widget/webview/checkout_webview.dart
@@ -1,1 +1,177 @@
+import 'dart:async';
+import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:jackdigitalstore_app/session/session.dart';
+import 'package:jackdigitalstore_app/utils/shared.dart';
+import 'package:jackdigitalstore_app/utils/utility.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class CheckoutWebView extends StatefulWidget {
+  final url;
+  final Future<dynamic> Function()? onFinish;
+  final bool fromOrder;
+  final bool withAppBar;
+
+  CheckoutWebView(
+      {Key? key,
+      this.url,
+      this.onFinish,
+      this.fromOrder = false,
+      this.withAppBar = true})
+      : super(key: key);
+  @override
+  CheckoutWebViewState createState() => CheckoutWebViewState();
+}
+
+class CheckoutWebViewState extends State<CheckoutWebView> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  WebViewController? _webViewController;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  _launchUrl(String url) async {
+    if (await canLaunchUrlString(url)) {
+      await launchUrlString(url);
+    } else {
+      snackBar(context, color: Colors.red, message: 'Could not launch $url');
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: widget.withAppBar
+            ? AppBar(
+                // backgroundColor: Colors.white,
+                title: Text(
+                  !widget.fromOrder
+                      ? AppLocalizations.of(context)!
+                          .translate("checkout_order")!
+                      : 'Payment Order',
+                  // style: TextStyle(color: Colors.black),
+                ),
+                leading: IconButton(
+                  // color: Colors.black,
+                  onPressed: () => Navigator.pop(context),
+                  icon: Platform.isIOS
+                      ? Icon(Icons.arrow_back_ios)
+                      : Icon(Icons.arrow_back),
+                ),
+              )
+            : null,
+        body: Stack(
+          children: [
+            WebView(
+              initialUrl: widget.url,
+              javascriptMode: JavascriptMode.unrestricted,
+              onProgress: (int progress) {
+                print("WebView is loading (progress : $progress%)");
+                if (progress != 100) {
+                  setState(() {
+                    isLoading = true;
+                  });
+                } else {
+                  setState(() {
+                    isLoading = false;
+                  });
+                }
+                _webViewController?.runJavascript(
+                    "document.getElementById('headerwrap').style.display= 'none';");
+                _webViewController?.runJavascript(
+                    "document.getElementById('footerwrap').style.display= 'none';");
+                _webViewController?.runJavascript(
+                    "document.getElementsByTagName('header')[0].style.display= 'none';");
+                _webViewController?.runJavascript(
+                    "document.getElementsByTagName('footer')[0].style.display= 'none';");
+                _webViewController?.runJavascript(
+                    "document.getElementsByClassName('return-to-shop')[0].style.display= 'none';");
+                _webViewController?.runJavascript(
+                    "document.getElementsByClassName('page-title')[0].style.display= 'none';");
+                _webViewController?.runJavascript(
+                    "document.getElementsByClassName('woocommerce-error')[0].style.display= 'none';");
+                _webViewController?.runJavascript(
+                    "document.getElementsByClassName('woocommerce-breadcrumb')[0].style.display= 'none';");
+                _webViewController?.runJavascript(
+                    "document.getElementsByClassName('entry-title')[0].style.display= 'none';");
+                _webViewController?.runJavascript(
+                    "document.getElementsByClassName('woocommerce-MyAccount-navigation')[0].style.display= 'none';");
+                _webViewController?.runJavascript(
+                    "document.getElementsByClassName('woo-wallet-sidebar')[0].style.display= 'none';");
+              },
+              onWebViewCreated: (WebViewController webViewController) {
+                _webViewController = webViewController;
+                _controller.complete(webViewController);
+              },
+              navigationDelegate: (NavigationRequest request) {
+                if (request.url.startsWith('gojek:')) {
+                  print('blocking navigation to $request}');
+                  _launchUrl(request.url);
+                  return NavigationDecision.prevent;
+                }
+                if (request.url.contains("/checkout/order-received/")) {
+                  final items = request.url.split("/checkout/order-received/");
+                  if (items.length > 1) {
+                    final number = items[1].split("/")[0];
+                    Session.data.setString('order_number', number);
+                  }
+                  if (!widget.fromOrder) {
+                    widget.onFinish!();
+                  } else {
+                    Navigator.pop(context, '200');
+                    snackBar(context,
+                        color: Colors.green, message: 'Payment Success');
+                  }
+                  return NavigationDecision.prevent;
+                }
+                print('allowing navigation to $request');
+                return NavigationDecision.navigate;
+              },
+              onPageStarted: (String url) {
+                print('Page started loading: $url');
+              },
+              onPageFinished: (String url) {
+                print('Page finished loading: $url');
+                _webViewController?.runJavascript(
+                    "document.getElementById('headerwrap').style.display= 'none';");
+                _webViewController?.runJavascript(
+                    "document.getElementById('footerwrap').style.display= 'none';");
+                _webViewController?.runJavascript(
+                    "document.getElementsByTagName('header')[0].style.display= 'none';");
+                _webViewController?.runJavascript(
+                    "document.getElementsByTagName('footer')[0].style.display= 'none';");
+                _webViewController?.runJavascript(
+                    "document.getElementsByClassName('return-to-shop')[0].style.display= 'none';");
+                _webViewController?.runJavascript(
+                    "document.getElementsByClassName('page-title')[0].style.display= 'none';");
+                _webViewController?.runJavascript(
+                    "document.getElementsByClassName('woocommerce-error')[0].style.display= 'none';");
+                _webViewController?.runJavascript(
+                    "document.getElementsByClassName('woocommerce-breadcrumb')[0].style.display= 'none';");
+                _webViewController?.runJavascript(
+                    "document.getElementsByClassName('entry-title')[0].style.display= 'none';");
+                _webViewController?.runJavascript(
+                    "document.getElementsByClassName('woocommerce-MyAccount-navigation')[0].style.display= 'none';");
+                _webViewController?.runJavascript(
+                    "document.getElementsByClassName('woo-wallet-sidebar')[0].style.display= 'none';");
+              },
+              gestureNavigationEnabled: true,
+            ),
+            isLoading
+                ? Center(
+                    child: customLoading(),
+                  )
+                : Stack(),
+          ],
+        ));
+  }
+}
